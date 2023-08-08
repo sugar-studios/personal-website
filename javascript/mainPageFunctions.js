7ea935daadc80b68e4062a3804f86a51e0b9274b
Alexander Calkins <abcalkins8723@gmail.com>
2023-08-08 14:35:23 -0700

    Redid Favicon

--- javascript/mainPageFunctions.js
+++ javascript/mainPageFunctions.js
@@ -1,20 +1,62 @@
 function Typer() {
-    const text = "Hello There!";
+    const messages = [
+        "Hello There!",
+        "¿que tal?",
+        ":)"
+    ];
     const animationSpeed = 50;
+    const pauseDuration = 7500; 
 
     const typingAnimation = document.getElementById("introText");
 
+    let currentMessageIndex = 0;
+    let isTyping = true;
     let i = 0;
+
     function typeText() {
-        if (i < text.length) {
-        typingAnimation.innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeText, animationSpeed);
+        if (isTyping && i < messages[currentMessageIndex].length) {
+            typingAnimation.innerHTML += messages[currentMessageIndex].charAt(i);
+            i++;
+            setTimeout(typeText, animationSpeed);
+        } else if (isTyping) {
+            isTyping = false;
+            setTimeout(untypeText, pauseDuration);
+        }
+    }
+
+    function untypeText() {
+        if (i > 0) {
+            typingAnimation.innerHTML = messages[currentMessageIndex].substring(0, i - 1);
+            i--;
+            setTimeout(untypeText, animationSpeed);
+        } else {
+            currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+            isTyping = true;
+            setTimeout(typeText, animationSpeed);
         }
     }
 
     typeText();
 }
+
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            Typer(); 
+            observer.unobserve(entry.target); 
+        }
+    });
+}
+
+
+const options = {
+    root: null, 
+    rootMargin: '0px',
+    threshold: 0.5 
+};
+
+const observer = new IntersectionObserver(handleIntersection, options);
+observer.observe(document.getElementById("introText"));
 
 
 function SlideAnimations() {
