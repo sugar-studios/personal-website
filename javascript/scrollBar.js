33f3e7e62fb5765d3c96ceeee1b8d1a4c49d9444
sugar studios <abcalkins8723@gmail.com>
2023-08-10 21:51:25 -0700

    Updated Transitions
    Need to fix scrollbar animations and middle section not showing

--- javascript/scrollBar.js
+++ javascript/scrollBar.js
@@ -1,50 +1,14 @@
-const canvas = document.getElementById('particle-canvas');
-const context = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-class Particle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.size = Math.random() * 5 + 1;
-    this.speedX = Math.random() * 3 - 1.5;
-    this.speedY = Math.random() * 3 - 1.5;
+window.addEventListener('scroll', function() {
+  const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollTop = window.scrollY;
+  
+  const scrollbarThumb = document.querySelector('::-webkit-scrollbar-thumb');
+  
+  if (scrollTop === 0 || scrollTop === scrollableHeight) {
+      scrollbarThumb.style.transform = 'scale(1.2) translateY(5px)';
+  } else {
+      scrollbarThumb.style.transform = 'scale(0.5) translateY(0)';
   }
-  update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.size > 0.2) this.size -= 0.1;
-  }
-  draw() {
-    context.fillStyle = 'white';
-    context.beginPath();
-    context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    context.fill();
-  }
-}
-
-const particles = [];
-
-function init() {
-  for (let i = 0; i < 50; i++) {
-    particles.push(new Particle(canvas.width / 2, canvas.height / 2));
-  }
-}
-
-function animate() {
-  requestAnimationFrame(animate);
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  for (const particle of particles) {
-    particle.update();
-    particle.draw();
-  }
-}
-
-init();
-animate();
-
-canvas.addEventListener('mousemove', (event) => {
-  const { clientX, clientY } = event;
-  particles.push(new Particle(clientX, clientY));
+  
+  scrollbarThumb.style.transition = 'transform 0.75s ease-out';
 });
