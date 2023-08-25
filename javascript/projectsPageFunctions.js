b13a53227fd10fb979e65d8d845a888fd8105cf7
sugar studios <abcalkins8723@gmail.com>
2023-08-24 20:30:02 -0700

    Added Images to paralax

---
+++ javascript/projectsPageFunctions.js
@@ -0,0 +1,20 @@
+function GetImage(ele) {
+    let IdImageEntries = '{"projects": [' +
+    '{"id": "p-EnterPresser", "image": "images/enterPresser.png"},'+
+    '{"id": "p-PortfolioWebsite", "image": "images/profile.png"},'+
+    '{"id": "p-ProjectA", "image": "images/project A.png"}]}';
+    const obj = JSON.parse(IdImageEntries);
+    let eleIndex;
+    for (let i = 0; i < obj.projects.length; i++) {
+        if (obj.projects[i].id === ele.id) { 
+            eleIndex = i;
+            break; 
+        }
+    }
+    if (eleIndex !== undefined) {
+        document.getElementById("projectContainer").style.backgroundImage = 'url(' + obj.projects[eleIndex].image + ')';
+    } else {
+        console.log("Project not found");
+    }
+}
+
